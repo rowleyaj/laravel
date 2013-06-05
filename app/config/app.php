9b6d4192b3c59722aae4bef7a6e211b6517da30f
Alex Rowley <rowleyaj@gmail.com>
2013-06-06 00:32:08 +0100

    Add GuardLaravel service provider

--- app/config/app.php
+++ app/config/app.php
@@ -114,6 +114,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'BCA\LaravelInspect\LaravelInspectServiceProvider',
+		'Way\Console\GuardLaravelServiceProvider',
 	),
 
 	/*
