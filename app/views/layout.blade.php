fd198d266bff8a052e3b82959a1c5158fd3ac993
Alex Rowley <rowleyaj@gmail.com>
2013-06-06 17:43:20 +0100

    Fix some messy intendation from merging files

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<!--[if IE 8]>			<html class="no-js lt-ie9" lang="en" > <![endif]-->
-<!--[if gt IE 8]><!-->	<html class="no-js" lang="en" > <!--<![endif]-->
+<!--[if IE 8]>          <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!-->  <html class="no-js" lang="en" > <!--<![endif]-->
 	<head>
 		<meta charset="utf-8" />
 		<title></title>
@@ -10,21 +10,21 @@
 
 		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-	  <link rel="stylesheet" href="assets/css/app.css" />
+		<link rel="stylesheet" href="assets/css/app.css" />
 	  
-	  <script src="assets/js/vendor/custom.modernizr.js"></script>
+		<script src="assets/js/vendor/custom.modernizr.js"></script>
 	</head>
 	<body>
 		@yield('content')
 
-	  <script>
-	  document.write('<script src=' +
-	  ('__proto__' in {} ? 'assets/js/vendor/zepto' : 'assets/js/vendor/jquery') +
-	  '.js><\/script>')
-	  </script>
+		<script>
+			document.write('<script src=' +
+			('__proto__' in {} ? 'assets/js/vendor/zepto' : 'assets/js/vendor/jquery') +
+			'.js><\/script>')
+		</script>
 	  
-		<!-- Full list of foundation JS, minify appropriate files -->	  
-	  <script src="assets/js/foundation/foundation.js"></script>		
+		<!-- Full list of foundation JS, minify appropriate files -->     
+		<script src="assets/js/foundation/foundation.js"></script>        
 		<script src="assets/js/foundation/foundation.alerts.js"></script>
 		<script src="assets/js/foundation/foundation.clearing.js"></script>
 		<script src="assets/js/foundation/foundation.cookie.js"></script>
@@ -40,19 +40,19 @@
 		<script src="assets/js/foundation/foundation.tooltips.js"></script>
 		<script src="assets/js/foundation/foundation.topbar.js"></script>
 
-	  <!-- Start Foundation -->
-	  <script>
-		$(document).foundation();
-	  </script>
+		<!-- Start Foundation -->
+		<script>
+			$(document).foundation();
+		</script>
 
-	  <!-- Google Analytics: change UA-XXXXX-X to be site's ID. -->
-    <script>
-      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-      e.src='//www.google-analytics.com/analytics.js';
-      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-      ga('create','UA-XXXXX-X');ga('send','pageview');
-    </script>
+		<!-- Google Analytics: change UA-XXXXX-X to be site's ID. -->
+		<script>
+			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+			e.src='//www.google-analytics.com/analytics.js';
+			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+  ga('create','UA-XXXXX-X');ga('send','pageview');
+		</script>
 	</body>
 </html>
